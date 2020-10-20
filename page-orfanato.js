a491500e7cb59c0a25df9ad8ce96f60525fba79d
Tales Gadelha <73200364+talesrgadelha@users.noreply.github.com>
2020-10-20 18:43:49 -0300

    Add files via upload

---
+++ page-orfanato.js
@@ -0,0 +1,9 @@
+const Mymap = L.map('mapid').setView([51.505, -0.09], 13);
+
+L.tileLayer('https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png', {
+    attribution: '&copy; <a href="https://www.openstreetmap.org/copyright">OpenStreetMap</a> contributors'
+}).addTo(Mymap);
+
+L.marker([51.5, -0.09]).addTo(Mymap)
+    .bindPopup('A pretty CSS3 popup.<br> Easily customizable.')
+    .openPopup();
